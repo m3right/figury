ec6da10d42adc76adf2042151e0ce3313401c182
Rafał Dreżewski <drezew@agh.edu.pl>
2021-11-24 22:08:58 +0100

    makefile

--- Punkt.java
+++ Punkt.java
@@ -1,27 +1,27 @@
 public class Punkt {
 	private double x, y;
-	
+
 	public Punkt(double nx, double ny) {
 		x = nx;
 		y = ny;
 	}
-	
+
 	public Punkt(Punkt p) {
 		x = p.x;
 		y = p.y;
 	}
-	
+
 	public double getX() { return x; }
 	public double getY() { return y; }
-	
+
 	public void setX(double nx) { x = nx; }
-	public void setY(double ny) { x = ny; }
-	
+	public void setY(double ny) { y = ny; }
+
 	public void przesun(double dx, double dy) {
 		x += dx;
 		y += dy;
 	}
-	
+
 	public String toString() {
 		return "(" + x + ", " + y + ")";
 	}
